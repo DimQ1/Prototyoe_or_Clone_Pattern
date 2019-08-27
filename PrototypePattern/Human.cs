fe622861c227a3dad9e15d07ee52508d80452c85
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-08-27 14:38:50 +0300

    Add project files.

---
+++ PrototypePattern/Human.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypePattern
+{
+    public class Human
+    {
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Adress { get; set; }
+        public string School { get; set; }
+        public string Mother { get; set; }
+        public string Father { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Nationality { get; set; }
+        public string FirstName { get; set; }
+
+        public Human Clone()
+        {
+            return this.MemberwiseClone() as Human;
+        }
+    }
+}
---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human kevin = new Human()
+            {
+                FirstName = "Kevin",
+                LastName = "Hart",
+                Nationality = "American",
+                City = "Philadelohia",
+                State = "PA",
+                Country = "USA",
+                Father = "Henry",
+                Mother = "Nancy",
+                School = "George Washington High School",
+                Adress = "123 Earth Way",
+                Gender = "Male"
+            };
+
+            Human robert = kevin.Clone();
+            robert.FirstName = "Robert";
+
+            Console.WriteLine($"{kevin.FirstName} {kevin.LastName}");
+            Console.WriteLine($"{robert.FirstName} {robert.LastName}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
